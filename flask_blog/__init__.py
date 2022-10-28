50071c4855e1ca71a09c1f0b56abb1b1b6380be2
Julia Didenko <iuliia.didenko@gmail.com>
2022-10-28 23:12:04 +0300

    Add Flask-Mail dependency, initialize extension in app

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,14 +1,23 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '00123344ed0172f2f4c3a4513b6fbecb'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASS')
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+mail = Mail(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
