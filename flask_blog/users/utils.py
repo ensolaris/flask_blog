21839ce18fcbb7127fb992bd65a7021b5beb8f58
Julia Didenko <iuliia.didenko@gmail.com>
2022-10-31 00:09:52 +0300

    Change project structure to add blueprints

---
+++ flask_blog/users/utils.py
@@ -0,0 +1,37 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for, current_app
+from flask_mail import Message
+from flask_blog import mail
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, file_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + file_ext
+    picture_path = os.path.join(
+        current_app.root_path,
+        'static/profile_pics',
+        picture_filename,
+    )
+    output_size = (125, 125)
+    resized_picture = Image.open(form_picture)
+    resized_picture.thumbnail(output_size)
+    resized_picture.save(picture_path)
+    return picture_filename
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message(
+        'Password Reset Request',
+        sender='noreply@demo.com',
+        recipients=[user.email]
+        )
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('users.reset_token', token=token, _external=True)}
+
+If you did not make this request, simply ignore this email, and no changes will be made.
+'''
+    mail.send(msg)
